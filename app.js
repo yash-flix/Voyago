71cdb3e29a46f6cf19fc63d9630b7154bf9dc2f6
Yash Rane <yash.tushar13@gmail.com>
2025-09-21 12:33:47 -0700

    added a custom error page

--- app.js
+++ app.js
@@ -124,7 +124,8 @@ app.use((req, res, next) => {
 app.use((err,req,res,next)=>
 {
     let {statusCode = 500 , message = "Something went wrong!"} = err;
-    res.status(statusCode).send(message);
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs");
 })
 
 
