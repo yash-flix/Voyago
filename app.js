6a64558699b93acca403cd0ad6b6c12497f1c721
Yash Rane <yash.tushar13@gmail.com>
2025-09-23 04:26:50 -0700

    used express router , and fixed /listing routes

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const app = express();
 const path = require('path');
 const Listing = require("./models/listing.js");
-// const methodOverride = require("method-override");
+const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const {lisitngSchema, listingSchema , reviewSchema} = require("./schema.js")
 const Review = require("./models/review.js");
-const methodOverride = require('method-override');
+
 
 // This must be BEFORE your routes
 app.use(methodOverride('_method'));
@@ -24,6 +24,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine("ejs" , ejsMate);
 
+const listings = require("./routes/listing.js")
 async function main()
 {
     await mongoose.connect(MONGO_URL);
@@ -43,20 +44,7 @@ app.get("/" , (req,res)=>
     res.render("listings/home.ejs");
 })
 
-const validateListing = (req,res,next)=>
-{
-let {error} = listingSchema.validate(req.body);
-         
 
-          if(error)
-          {
-            let errMsg = error.details.map((el)=> el.message).join(",");
-            throw new ExpressError(400, errMsg);
-          }
-          else{
-            next();
-          }
-}
 
 const validateReview = (req,res,next)=>
 {
@@ -86,64 +74,7 @@ let {error} = reviewSchema.validate(req.body);
 //     res.send("succes");
 // })
 
-//index route
-app.get("/listings" ,wrapAsync( async (req,res)=>
-{
-   const allListings =  await Listing.find({})
-   res.render("listings/index.ejs" , {allListings});
-}));
-
-//new route
-app.get("/listings/new" , (req,res)=>
-{
-    res.render("listings/new.ejs")
-})
-
-//show route
-app.get("/listings/:id", wrapAsync(async(req,res)=>
-{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", {listing});
-}));
-
-//create route
-app.post("/listings" , validateListing , wrapAsync( async (req,res , next)=>
-{
-          
-        // let listing = req.body.listing;
-       const newListing = new Listing(req.body.listing); //destructing data and parcing into values from objects
-       await newListing.save();
-       res.redirect("/listings")
-    
-}));
-
-//edit route
-app.get("/listings/:id/edit" , wrapAsync(async (req,res)=>
-{
-   let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs" , {listing}) 
-})); 
-
-//edit/update route
-app.put("/listings/:id" , validateListing ,wrapAsync(async (req,res)=>
-{
-    let {id} = req.params;
-   await Listing.findByIdAndUpdate(id , {...req.body.listing});
-   res.redirect(`/listings/${id}`);
-
-
-}));
-
-//delete route
-app.delete("/listings/:id" , wrapAsync(async (req,res)=>
-{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing  )
-    res.redirect("/listings")
-}));
+app.use("/listings" , listings )
 
 
 //POST review route 
@@ -168,7 +99,7 @@ app.post("/listings/:id/reviews" , validateReview, wrapAsync(async (req,res)=>
 app.delete("/listings/:id/reviews/:reviewId" , wrapAsync(async (req,res)=>
 {
     let {id , reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id , {$pull: {reviews : reviewId}}) 
+    await Listing.findByIdAndUpdate(id , {$pull: {reviews : reviewId}}) //pull is used to extract a value that is to be removed
     await Review.findByIdAndDelete(reviewId);
     res.redirect(`/listings/${id}`);
 
